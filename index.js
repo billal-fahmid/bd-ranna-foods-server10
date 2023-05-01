c53387a643680f9da37650d18d2d720a03993bdf
billal-fahmid <billalcooom@gmail.com>
2023-05-02 00:29:04 +0600

    chef details data creaet done

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', (req,res)=>{
-    res.send({name:'Billal Hossen'})
+const chefs = require('./chefs.json')
+
+app.get('/chefs', (req,res)=>{
+    res.send(chefs);
 })
+
+app.get('/chefs/:id', (req, res) =>{
+    const id =req.params.id;
+    const chef = chefs.find(cf => cf.chefId == id);
+    res.send(chef)
+})
+
 
 app.listen(port,() =>{
     console.log('server is running port no is ' , port)
