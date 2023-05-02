2c160f74d2dea67a1e2c3868506db9ffe9a60588
billal-fahmid <billalcooom@gmail.com>
2023-05-02 11:30:04 +0600

    chef recipes data make done

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require('express');
+const cors = require('cors')
 
 const app = express();
 const port = 5000;
 
+app.use(cors())
+
 const chefs = require('./chefs.json')
+const recipes = require('./recipes.json')
 
 app.get('/chefs', (req,res)=>{
     res.send(chefs);
 })
 
+
 app.get('/chefs/:id', (req, res) =>{
     const id =req.params.id;
     const chef = chefs.find(cf => cf.chefId == id);
     res.send(chef)
+})
+
+app.get('/recipes', (req, res) =>{
+    res.send(recipes);
+})
+
+app.get('/recipes/:id' , (req,res) =>{
+    const id = req.params.id;
+    const chefRecipes = recipes.filter(recipe => recipe.chefId == id);
+    // console.log(chefRecipes)
+    res.send(chefRecipes)
 })
 
 
